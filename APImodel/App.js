aa394fb255b485843e9c270cd5cb7e9e95511eb0
winfred <wskagbakpe@st.ug.edu.gh>
2024-07-08 21:50:02 +0000

    drawer navigation implemented

---
+++ APImodel/App.js
@@ -0,0 +1,35 @@
+import 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
+import CartScreen from './CartScreen';
+const Stack = createStackNavigator();
+const Drawer= createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home"  screenOptions={{
+          headerShown: false,
+        }}>
+         <Drawer.Screen name="Store" component={HomeScreen} />
+         <Drawer.Screen name="Locations" component={CartScreen} />
+         <Drawer.Screen name="Blog" component={CartScreen} />
+         <Drawer.Screen name="Jewelery" component={CartScreen} />
+         <Drawer.Screen name="Electronics" component={CartScreen} />
+         <Drawer.Screen name="Clothing" component={CartScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
